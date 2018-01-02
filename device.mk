971d27fc408fa89ce96b0096d84453984308b091
yatto <yattodettaman@hotmail.com>
2018-01-02 10:15:55 +0100

    Duma: use XML audio policy configuration
    Change-Id: I2b76b9edf39d48f9b89db440ef35201e81d14c37

--- device.mk
+++ device.mk
@@ -92,9 +92,17 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     libaudio-resampler
 
+# Audio configuration
+USE_XML_AUDIO_POLICY_CONF := 1
+
 PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     device/asus/duma/audio/audio_effects_vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects_vendor.conf \
-    device/asus/duma/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
+    device/asus/duma/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     device/asus/duma/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Get the long list of APNs
