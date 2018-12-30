122f13da5b113da3b0155c61129c37ef126d1080
Den147 <den14712@gmail.com>
2018-12-30 14:42:30 +0300

    duma: enable offline charger
    Signed-off-by: Den147 <den14712@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,11 +73,11 @@ TARGET_RIL_VARIANT := caf
 TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
 
 # Charger 
-#BOARD_CHARGER_DISABLE_INIT_BLANK := true
-#BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(PLATFORM_PATH)/charger/images
-#BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
-#RED_LED_PATH := /sys/class/leds/led:red/brightness
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+RED_LED_PATH := /sys/class/leds/led:red/brightness
 
 # Audio
 AUDIO_FEATURE_ENABLED_VORBIS_OFFLOAD := true
