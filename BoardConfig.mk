9b11afe299aa7a7ff3e90cea50077ec46ccc9c0a
Den147 <den14712@gmail.com>
2020-01-29 14:14:14 +0300

    duma: set TARGET_CPU_VARIANT_RUNTIME

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,8 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := krait
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := krait
 
 # Snapdragon LLVM
 
